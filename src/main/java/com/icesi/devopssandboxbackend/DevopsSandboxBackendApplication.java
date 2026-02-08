2f116f02b6e4f4ab069b0013e74e7a37a0432d40
Alejoamu <alejoamu2@gmail.com>
2026-02-08 16:29:48 -0500

    update: main method added

--- src/main/java/com/icesi/devopssandboxbackend/DevopsSandboxBackendApplication.java
+++ src/main/java/com/icesi/devopssandboxbackend/DevopsSandboxBackendApplication.java
@@ -2,14 +2,10 @@ package com.icesi.devopssandboxbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class DevopsSandboxBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsSandboxBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsSandboxBackendApplication.class, args);
+    }
 }
-
